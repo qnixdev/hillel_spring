c7030d7848550dfee7b99d960dd9e60b343923c5
qnixdev <qnixdev@gmail.com>
2022-06-17 13:09:41 +0300

    fix models

--- src/main/java/com/hillel_spring/model/Animal.java
+++ src/main/java/com/hillel_spring/model/Animal.java
@@ -2,9 +2,11 @@ package com.hillel_spring.model;
 
 import lombok.Builder;
 import lombok.Data;
+import lombok.ToString;
 
 @Builder
 @Data
+@ToString
 public class Animal {
     private Integer id;
     private String name;
