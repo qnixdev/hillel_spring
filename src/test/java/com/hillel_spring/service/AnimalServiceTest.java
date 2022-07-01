cb7e73d694bb4835bfd67be8408e02b74a1cad39
qnixdev <qnixdev@gmail.com>
2022-07-01 11:26:16 +0300

    hw25

---
+++ src/test/java/com/hillel_spring/service/AnimalServiceTest.java
@@ -0,0 +1,72 @@
+package com.hillel_spring.service;
+
+import com.hillel_spring.model.Animal;
+import com.hillel_spring.model.Person;
+import com.hillel_spring.repository.AnimalRepository;
+import com.hillel_spring.repository.PersonRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import java.util.Optional;
+
+public class AnimalServiceTest {
+    private final PersonRepository personRepository = Mockito.mock(PersonRepository.class);
+    private final AnimalRepository animalRepository = Mockito.mock(AnimalRepository.class);
+    private final PersonService personService = new PersonService(this.personRepository);
+    private final AnimalService animalService = new AnimalService(this.animalRepository, this.personService);
+
+    @Test
+    public void testCreate() {
+        var dog = Animal.builder().id(123).name("Pitbull").personId(6).build();
+        var dogOwner = Person.builder().id(333).name("Mykola").build();
+
+        Mockito
+            .when(this.personRepository.findById(Mockito.anyInt()))
+            .thenReturn(Optional.of(dogOwner))
+        ;
+        Mockito
+            .when(this.animalRepository.save(Mockito.any()))
+            .thenReturn(dog)
+        ;
+
+        Assertions.assertEquals(dog, this.animalService.create(dog));
+        Mockito.verify(this.personRepository).findById(Mockito.anyInt());
+        Mockito.verify(this.animalRepository).save(dog);
+    }
+
+    @Test
+    public void testRead() {
+        var dog = Animal.builder().id(123).name("Pitbull").personId(6).build();
+
+        Mockito
+            .when(this.animalRepository.findById(Mockito.anyInt()))
+            .thenReturn(Optional.of(dog))
+        ;
+
+        Assertions.assertEquals(dog, this.animalService.read(Mockito.anyInt()));
+        Mockito.verify(this.animalRepository).findById(Mockito.anyInt());
+    }
+
+    @Test
+    public void testUpdate() {
+        var receivedDog = Animal.builder().id(123).name("Pitbull").personId(6).build();
+        var existDog = Animal.builder().id(123).name("Pitbull").personId(777).build();
+
+        Mockito
+            .when(this.animalRepository.findById(Mockito.any()))
+            .thenReturn(Optional.of(existDog))
+        ;
+
+        Assertions.assertTrue(this.animalService.update(receivedDog));
+        Mockito.verify(this.animalRepository).save(receivedDog);
+    }
+
+    @Test
+    public void testDelete() {
+        var dog = Animal.builder().id(123).name("Pitbull").personId(6).build();
+
+        this.animalService.delete(dog.getId());
+
+        Mockito.verify(this.animalRepository).deleteById(Mockito.anyInt());
+    }
+}
---
+++ src/test/java/com/hillel_spring/service/PersonServiceTest.java
@@ -0,0 +1,62 @@
+package com.hillel_spring.service;
+
+import com.hillel_spring.model.Person;
+import com.hillel_spring.repository.PersonRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import java.util.Optional;
+
+public class PersonServiceTest {
+    private final PersonRepository personRepository = Mockito.mock(PersonRepository.class);
+    private final PersonService personService = new PersonService(this.personRepository);
+
+    @Test
+    public void testCreate() {
+        var person = Person.builder().id(321).name("Peter").build();
+
+        Mockito
+            .when(this.personRepository.save(Mockito.any()))
+            .thenReturn(person)
+        ;
+
+        Assertions.assertEquals(person, this.personService.create(person));
+        Mockito.verify(this.personRepository).save(person);
+    }
+
+    @Test
+    public void testRead() {
+        var person = Person.builder().id(321).name("Peter").build();
+
+        Mockito
+            .when(this.personRepository.findById(Mockito.anyInt()))
+            .thenReturn(Optional.of(person))
+        ;
+
+        Assertions.assertEquals(person, this.personService.read(Mockito.anyInt()));
+        Mockito.verify(this.personRepository).findById(Mockito.anyInt());
+    }
+
+    @Test
+    public void testUpdate() {
+        var receivedPerson = Person.builder().id(321).name("Peter").build();
+        var existPerson = Person.builder().id(321).name("Peter Parker").build();
+
+        Mockito
+            .when(this.personRepository.findById(Mockito.any()))
+            .thenReturn(Optional.of(existPerson))
+        ;
+
+        Assertions.assertTrue(this.personService.update(receivedPerson));
+        Mockito.verify(this.personRepository).save(receivedPerson);
+    }
+
+    @Test
+    public void testDelete() {
+        var person = Person.builder().id(321).name("Peter").build();
+
+        this.personService.delete(person.getId());
+
+        Mockito.verify(this.personRepository).deleteById(Mockito.anyInt());
+    }
+}
